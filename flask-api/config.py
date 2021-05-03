95217ae84f8ecab3ae12716e011097bbbbf94304
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-05-03 17:24:39 -0300

    fix: :bug: fixed mysql host

--- flask-api/config.py
+++ flask-api/config.py
@@ -1,11 +1,11 @@
 SECRET_KEY = "log_parser"
 
 MYSQL_ROOT_PASSWORD: "devpasswordlogparser"
-MYSQL_HOST = "localhost"
+MYSQL_HOST = "database"
 MYSQL_USER = "flask"
 MYSQL_PASSWORD = "devpasswordlogparser"
 MYSQL_DB = "log_parser"
-MYSQL_PORT = 5432
+MYSQL_PORT = 3305
 
 SQLALCHEMY_DATABASE_URI = (
     f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
