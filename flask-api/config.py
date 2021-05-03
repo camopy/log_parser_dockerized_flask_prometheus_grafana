a47023db96eec0b7e42e4eae154a2f04e34cf15c
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-05-03 19:23:02 -0300

    fix: :bug: fixed mysql container connection with flask

--- flask-api/config.py
+++ flask-api/config.py
@@ -5,7 +5,7 @@ MYSQL_HOST = "database"
 MYSQL_USER = "flask"
 MYSQL_PASSWORD = "devpasswordlogparser"
 MYSQL_DB = "log_parser"
-MYSQL_PORT = 3305
+MYSQL_PORT = 3306
 
 SQLALCHEMY_DATABASE_URI = (
     f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
--- flask-api/main.py
+++ flask-api/main.py
@@ -7,7 +7,7 @@ db = SQLAlchemy(app)
 
 from views import *
 
-# db.create_all()
+db.create_all()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
