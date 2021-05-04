c93be9c89cf4d30e115e06d5a08f731ea8233bf2
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-05-03 22:56:57 -0300

    feat: :sparkles: added template html to the flask project

--- flask-api/views.py
+++ flask-api/views.py
@@ -1,3 +1,4 @@
+from flask import render_template, redirect, url_for
 from parser import parse_log_file
 from main import app, db
 from models import File
@@ -5,6 +6,17 @@ from models import File
 
 @app.route("/")
 def index():
+    return render_template("index.html", title="Log Parser")
+
+
+@app.route("/rsync")
+def rsync():
+    logs = db.session.query(File).all()
+    return render_template("rsync/logs.html", title="Rsync Logs", logs=logs)
+
+
+@app.route("/rsync/parse")
+def rsync_logs_parse():
     files = parse_log_file()
 
     for file in files:
@@ -17,7 +29,17 @@ def index():
         db.session.add(file)
 
     db.session.commit()
-    return "Parsing..."
+    return redirect(url_for("rsync"))
+
+
+@app.route("/prometheus")
+def prometheus():
+    return redirect("http://localhost:9090")
+
+
+@app.route("/grafana")
+def grafana():
+    return redirect("http://localhost:3000")
 
 
 @app.route("/api/rsync/logs_count")
